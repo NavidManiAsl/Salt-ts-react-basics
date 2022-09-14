e85a9a7d02e78a8a4ab73486f0c06845736ffefa
NavidManiAsl <navid.maniasl@outlook.com>
2022-09-14 16:14:24 +0200

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import {ChangeEvent, FC, useEffect, useState}from 'react';
+import {ChangeEvent, FC, useEffect, useRef, useState}from 'react';
 import './App.css';
 import User from './components/User';
 
 const App:FC = () => {
   
+  const [name, setName] = useState('')
+  const [age, setAge] = useState(0)
+  const [address, setAddress] = useState('Never land')
+  const dataFetchedRef = useRef(false);
+
   const getUser = async ()=>{
     const res =await fetch('https://random-data-api.com/api/v2/users?size=1&is_xml=true')
     const data = await res.json()
-    return data
+    setName(data.first_name)
+    setAge(data.id)  // the api does not support the age so id has been used instead
+    setAddress(data.address.state)
+ 
+    
   }
-  const [name, setName] = useState('')
-  const [age, setAge] = useState(0)
-  const [address, setAddress] = useState('helvetica')
   const nameChanger =(e:ChangeEvent<HTMLInputElement>)=>{
       setName(e.target.value)
   }
   useEffect(  () => {
-    getUser()
-      .then(data => {
-        setName(data.first_name)
-        setAge(data.id)  // the api does not support the age so id has been used instead
-        setAddress(data.address.state)
-    }) 
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+    getUser()    
   }, [])
   
   return (
