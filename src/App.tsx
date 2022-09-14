30aba1c7e7c1c87cf9305eda5e36cadb4fd9097d
NavidManiAsl <navid.maniasl@outlook.com>
2022-09-14 15:44:31 +0200

    add fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import {ChangeEvent, FC, useState}from 'react';
+import {ChangeEvent, FC, useEffect, useState}from 'react';
 import './App.css';
 import User from './components/User';
+const getUser = async ()=>{
+  const data =await fetch('https://random-data-api.com/api/v2/users?size=1&is_xml=true')
+  return data.json()
+}
 
 const App:FC = () => {
   
   const [name, setName] = useState('')
   const [age, setAge] = useState(0)
-  const [address, setAddress] = useState('')
+  const [address, setAddress] = useState('helvetica')
   const nameChanger =(e:ChangeEvent<HTMLInputElement>)=>{
       setName(e.target.value)
   }
+  useEffect( () => {
+    getUser().then(data => {
+      console.log(data)
+      setName(data.first_name)
+      setAge(data.id)
+      setAddress(data.address.state)
+    }) 
+  }, [])
+  
   return (
     <div className="App">
      <User  name={name} age={age} address={address} />
